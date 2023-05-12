d71b45689eb77963877be2a9006a393c71ecdb6a
René Faßbender <rene-fassb@gmx.de>
2023-05-12 11:34:22 +0200

    feat: answerbutton functionality

--- js.js
+++ js.js
@@ -1,4 +1,5 @@
 const cards = document.querySelectorAll('[data-js="card"]');
+const answerTexts = document.querySelectorAll('[data-js="card__answertext"]');
 
 // Get all the bookmark elements
 const bookmarks = document.querySelectorAll('[data-js="card__bookmark"]');
@@ -16,6 +17,31 @@ bookmarks.forEach((bookmark) => {
     } else {
       // If it's the active version, change it back to the default version
       bookmark.setAttribute("src", "./images/bookmark.svg");
+    }
+  });
+});
+
+// Get all buttonanswer elements and save them in buttonsAnswer
+const buttonsAnswer = document.querySelectorAll(
+  '[data-js="card__buttonanswer"]'
+);
+
+//loop through each buttonAnswer element
+buttonsAnswer.forEach((buttonAnswer) => {
+  //add Event on click on the corresponding button
+  buttonAnswer.addEventListener("click", () => {
+    //choose from the parent element of the button the answertext p element
+    const answerText = buttonAnswer.parentElement.querySelector(
+      '[data-js="card__answertext"]'
+    );
+    //catch the attribute hidden from answertext if it != null
+    const isHidden = answerText.getAttribute("hidden") !== null;
+    //if it is hidden remove the attribute
+    if (isHidden) {
+      answerText.removeAttribute("hidden");
+      //else add it
+    } else {
+      answerText.setAttribute("hidden", "");
     }
   });
 });
